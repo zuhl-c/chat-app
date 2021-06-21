58713f40ee19badbdf3b3162692119e01ba1958b
zuhl.c <zuhailzed@gmail.com>
2021-06-21 12:39:47 +0530

    time , code , modification

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/* this program was written by zuhail pm */
+/* for more info : www.github.com/zuhl-c */
+
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
@@ -5,22 +8,10 @@ const port = 3000;
 const io = require('socket.io')(server);
 const path = require('path');
 const fs = require('fs');
+var date=require('date-and-time');
+
 
 app.use(express.static(__dirname + '/public'));
-
-io.on('connection',(socket)=>{
-
-    console.log('client connected');
-    socket.on('disconnect',()=>{
-        console.log('client disconnected');
-    });
-    socket.on('chat message',(msg)=>{
-        console.log('client : '+msg);
-
-        socket.emit('chat message','Hi from server');
-        //socket.broadcast.emit('chat message',msg);
-    });
-});
 
 app.get('/',function(req,res){
     res.sendFile('./index.html',{root:__dirname});
@@ -29,3 +20,38 @@ app.get('/',function(req,res){
 server.listen(port,function(){
     console.log('server running : '+port);
 });
+
+var clients=[];
+
+io.on('connection',(socket)=>{
+
+    var client_info= new Object();
+    client_info.id=socket.id;
+    clients.push(client_info);
+    console.log('total clients => ' +clients.length );console.log(clients);
+
+    console.log('client connected : id : '+socket.id);
+    socket.on('disconnect',()=>{
+        console.log('client disconnected : id : '+socket.id);
+        deleteId(socket.id);
+    });
+
+    socket.on('chat message',async(msg)=>{
+        var time = date.format( new Date() ,'hh:mm A');
+        console.log('client : ' +socket.id +' , message : '+msg+' , time : '+time);
+        io.emit('chat message',msg,socket.id,time);
+       
+    });
+});
+
+async function deleteId(id){
+    for(var i =0; i<clients.length;i++){
+       // console.log(clients[i]);
+        if(clients[i].id==id){
+            // console.log('removing disconnceted client id '+clients[i].id);
+            await clients.splice(i,1);
+            console.log('disconnected client id has been deleted');
+            break;
+        }
+    }
+}
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,23 +1,42 @@
 const socket = io("ws://localhost:3000");
-function sendMsg(){
-  socket.emit('chat message',$('#msg').val());
-  sendmessage($('#msg').val())
-  $('#msg').val('');
-  return 0;
-}
-socket.on('chat message',(msg)=>{
-    renderMessage(msg);
-  })
-var renderMessage = message => {
-var div = document.createElement('div')
-div.classList.add('render-message')
-div.innerText = message
-document.getElementById('chat-box').appendChild(div)
-}
-var sendmessage = message => {
-var div = document.createElement('div')
-div.classList.add('send-message')
-div.innerText = message
-document.getElementById('chat-box').appendChild(div)
 
+function sendMsg(){
+  var message = $('#msg').val();
+  if(message!=''){
+    socket.emit('chat message',message);
+    $('#msg').val('');
+    return 0;
+  }else{
+    alert('message cannot empty');
+  }
+  
+}
+socket.on('chat message',(msg,id,time)=>{
+  console.log(id);
+  if(socket.id==id){
+    sendmessage(msg,time);
+  }else{
+    renderMessage(msg,time);
+  }
+  })
+var renderMessage = (message,time) => {
+var div=document.createElement('div');
+var h5 = document.createElement('h5');
+h5.classList.add('time');
+div.classList.add('render-message');
+div.innerText = message;
+h5.innerText= time;
+document.getElementById('chat-box').appendChild(div);
+document.getElementById('chat-box').appendChild(h5);
+}
+
+var sendmessage = (message,time) => {
+var div = document.createElement('div');
+var h5 = document.createElement('h5');
+h5.classList.add('time-after');
+div.classList.add('send-message');
+div.innerText = message;
+h5.innerText= time;
+document.getElementById('chat-box').appendChild(div);
+document.getElementById('chat-box').appendChild(h5);
 }
